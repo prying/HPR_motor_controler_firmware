a06e9c42b58c687389d26ce818f8d5aaa078e060
Flynn <flynn@belmontharrison.net>
2022-07-11 23:49:04 +1000

    added current state feedback

--- Core/Src/fsm.c
+++ Core/Src/fsm.c
@@ -51,81 +51,86 @@ eFsmEvent nextEventFromState(eFsmState state);
 // When an error is detected call Error_Event and go to the aborted state
 eFsmState _errorHandler(eFsmPeripheriesData *sPeripheries)
 {
-    // TODO turn everything off and to the safe position
-    // Turn off igniter and power to the ignition sorce
-    // Turn off IGN Control
-    HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_RESET);
+  // TODO turn everything off and to the safe position
+  // Turn off igniter and power to the ignition sorce
+  // Turn off IGN Control
+  HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_RESET);
 
-    // Turn on the buck converter for the igniter
-    HAL_Delay(IGN_CONTROL_OFF_DELAY);
-    HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_RESET);
+  // Turn on the buck converter for the igniter
+  HAL_Delay(IGN_CONTROL_OFF_DELAY);
+  HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_RESET);
 
-    // Move valve to suitable position
-    // TODO pwm control
+  // Move valve to suitable position
+  // TODO pwm control
 
-    // Anounce over usart and i2c to let everyone know
-    HAL_UART_Transmit_DMA(&huart2, (uint8_t *)errorMsg, sizeof(errorMsg) / sizeof(char));
-    // TODO Some i2c related method
-    return Aborted_State;
+  // Anounce over usart and i2c to let everyone know
+  HAL_UART_Transmit_DMA(&huart2, (uint8_t *)errorMsg, sizeof(errorMsg) / sizeof(char));
+  // TODO Some i2c related method
+  return Aborted_State;
 }
 
 // Go from idle to standby
 eFsmState _recivedArmHandler(eFsmPeripheriesData *sPeripheries)
 {
-    // TODO turn on pwr supply for igiter (i.e. turn on the buck converter)
-    // Turn on the buck converter for the igniter
-    HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_SET);
-    return Standby_State;
+  // TODO turn on pwr supply for igiter (i.e. turn on the buck converter)
+  // Turn on the buck converter for the igniter
+  HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_SET);
+  return Standby_State;
 }
 
 // Turn igniter on
 eFsmState _revicedLaunchHandler(eFsmPeripheriesData *sPeripheries)
 {
-    // TODO turn on mosfet to allow current to travel to the igiter
-    HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_SET);
-    return Igniter_On_State;
+  // TODO turn on mosfet to allow current to travel to the igiter
+  HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_SET);
+
+  // TODO: start timer that will trigger Open_Valve_Timer_Event
+  return Igniter_On_State;
 }
 
 // Timer finishes counting to open clock
 eFsmState _alarmOpenValveHandler(eFsmPeripheriesData *sPeripheries)
 {
-    PWM1_setPos(SERVO_OPEN_ANGLE);
-    return Valve_Open_State;
+  PWM1_setPos(SERVO_OPEN_ANGLE);
+
+  // TODO: Start timer that will trigger Stop_Igniter_Timer_Event
+  
+  return Valve_Open_State;
 }
 
 // Timer finishes counting to turn igniter off
 eFsmState _alarmTurnOffIgniterHandler(eFsmPeripheriesData *sPeripheries)
 {
-    // TODO turn off mosfet and turn off pwr supply for igniter
-    // Turn off IGN Control
-    HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_RESET);
+  // TODO turn off mosfet and turn off pwr supply for igniter
+  // Turn off IGN Control
+  HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_RESET);
 
-    // Turn on the buck converter for the igniter
-    HAL_Delay(IGN_CONTROL_OFF_DELAY);
-    HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_RESET);
+  // Turn on the buck converter for the igniter
+  HAL_Delay(IGN_CONTROL_OFF_DELAY);
+  HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_RESET);
 
-    return Igniter_Off_State;
+  return Igniter_Off_State;
 }
 
 // When in the aborted state it can be reset
 eFsmState _resetHandler(eFsmPeripheriesData *sPeripheries)
 {
-    // TODO move all vars needed to be in the idle state
+  // TODO move all vars needed to be in the idle state
 
-    // Turn off IGN Control
-    HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_RESET);
+  // Turn off IGN Control
+  HAL_GPIO_WritePin(IGN_CONTROL_GPIO_Port, IGN_CONTROL_Pin, GPIO_PIN_RESET);
 
-    // Turn on the buck converter for the igniter
-    HAL_Delay(IGN_CONTROL_OFF_DELAY);
-    HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_RESET);
+  // Turn on the buck converter for the igniter
+  HAL_Delay(IGN_CONTROL_OFF_DELAY);
+  HAL_GPIO_WritePin(IGN_PWR_GPIO_Port, IGN_PWR_Pin, GPIO_PIN_RESET);
 
-    // Move valve to suitable position
-    PWM1_setPos(SERVO_CLOSED_ANGLE);
+  // Move valve to suitable position
+  PWM1_setPos(SERVO_CLOSED_ANGLE);
 
-    // Anounce over usart and i2c to let everyone know
-    HAL_UART_Transmit_DMA(&huart2, (uint8_t *)resetMsg, sizeof(resetMsg) / sizeof(char));
+  // Anounce over usart and i2c to let everyone know
+  HAL_UART_Transmit_DMA(&huart2, (uint8_t *)resetMsg, sizeof(resetMsg) / sizeof(char));
 
-    return Idle_State;
+  return Idle_State;
 }
 
 // Public function
@@ -134,22 +139,22 @@ eFsmState _resetHandler(eFsmPeripheriesData *sPeripheries)
 // Initialize finite state machine
 void FSM_init()
 {
-    eFsmCurrentState = Idle_State;
-    return;
+  eFsmCurrentState = Idle_State;
+  return;
 }
 
 // Get current state;
 eFsmState FSM_state()
 {
-    return eFsmCurrentState;
+  return eFsmCurrentState;
 }
 
 // Get event
 void FSM_sendEvent(eFsmEvent Event)
 {
-    // TODO! validation ect;
-    eFsmNewEvent = Event;
-    return;
+  // TODO! validation ect;
+  eFsmNewEvent = Event;
+  return;
 }
 
 // Step the finite state machines logic
@@ -160,25 +165,25 @@ void FSM_step(eFsmPeripheriesData *sPeripheries)
   // Setup linkages for the FSM               Might make a global if it is wasiting alot of loop resourses?
   static afEventHandler FSM =
   {
-      [Idle_State]        = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Arm_Event]                = _recivedArmHandler          },
-      [Standby_State]     = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Launch_Event]             = _revicedLaunchHandler       },
-      [Igniter_On_State]  = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Open_Valve_Timer_Event]   = _alarmOpenValveHandler      },
-      [Valve_Open_State]  = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Stop_Igniter_Timer_Event] = _alarmTurnOffIgniterHandler },
-      [Igniter_Off_State] = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler                                                          },
-      [Aborted_State]     = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler                                                          }
+    [Idle_State]        = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Arm_Event]                = _recivedArmHandler          },
+    [Standby_State]     = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Launch_Event]             = _revicedLaunchHandler       },
+    [Igniter_On_State]  = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Open_Valve_Timer_Event]   = _alarmOpenValveHandler      },
+    [Valve_Open_State]  = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler, [Stop_Igniter_Timer_Event] = _alarmTurnOffIgniterHandler },
+    [Igniter_Off_State] = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler                                                          },
+    [Aborted_State]     = {[Error_Event] = _errorHandler, [Reset_Event]  = _resetHandler                                                          }
   };
 
   // Validate that both state and event are valid and that there is a event handler at the event for this state
   if ((eFsmCurrentState < Last_State) && (eFsmNewEvent < Last_Event) && FSM[eFsmCurrentState][eFsmNewEvent] != NULL && eFsmNewEvent != preEvent)
   {
-      // Call the event handler at the end of the function pointer
-      eFsmCurrentState = (*FSM[eFsmCurrentState][eFsmNewEvent])(sPeripheries);
-      preEvent = eFsmNewEvent;
-      sendStateMsg(eFsmCurrentState);
+    // Call the event handler at the end of the function pointer
+    eFsmCurrentState = (*FSM[eFsmCurrentState][eFsmNewEvent])(sPeripheries);
+    preEvent = eFsmNewEvent;
+    sendStateMsg(eFsmCurrentState);
   }
   else
   {
-      //TODO Maybe this could also trigger and abort???
+    //TODO Maybe this could also trigger and abort???
   }
 
 
@@ -222,27 +227,27 @@ void FSM_reciveCMD(UART_HandleTypeDef * uartHandle)
 // Anounce on UART that it has moved to a state
 void sendStateMsg(eFsmState state)
 {
-    // Needs to be a static becuase the memory is deallocated when this function is removed from the stack before DMA has finished moving it
-    static char msgBuff[MSGBUFF_SIZE];
-    int n = 0;
+  // Needs to be a static becuase the memory is deallocated when this function is removed from the stack before DMA has finished moving it
+  static char msgBuff[MSGBUFF_SIZE];
+  int n = 0;
 
-    // TODO: make safe with snprintf
-    n = sprintf(msgBuff, "State: %s\n", eFsmStateNames[state]);
-    if (n <= 0)
+  // TODO: make safe with snprintf
+  n = sprintf(msgBuff, "State: %s\n", eFsmStateNames[state]);
+  if (n <= 0)
+  {
+    // Somthing went wrong BUT DONT CRASH
+  }
+  else
+  {
+    // Cursed code TODO: figure out why for reset and error event code get stuck on 33 (busy tx-ing)
+    huart2.gState = HAL_UART_STATE_READY;
+    if (HAL_UART_Transmit(&huart2, (uint8_t *)msgBuff, n, 100) != HAL_OK)
     {
-        // Somthing went wrong BUT DONT CRASH
-    }
-    else
-    {
-      // Cursed code TODO: figure out why for reset and error event code get stuck on 33 (busy tx-ing)
-      huart2.gState = HAL_UART_STATE_READY;
-      if (HAL_UART_Transmit(&huart2, (uint8_t *)msgBuff, n, 100) != HAL_OK)
-      {
 
-      }
     }
+  }
 
-    return;
+  return;
 }
 
 eFsmEvent nextEventFromState(eFsmState state)
